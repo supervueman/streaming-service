61a441e15957bcc7fb11c7eb8ff92ea10d0fb4b3
supervueman <supervueman@gmail.com>
2019-05-31 20:51:30 +0400

    Start template

---
+++ dev/src/App.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  v-app
+    v-toolbar(app)
+      v-toolbar-title(class="headline text-uppercase")
+        span Vuetify
+        span(class="font-weight-light") MATERIAL DESIGN
+      v-spacer
+      v-btn(flat href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank")
+        span(class="mr-2") Latest Release
+    v-content
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+  data() {
+    return {
+      //
+    };
+  }
+};
+</script>
---
+++ dev/src/main.js
@@ -0,0 +1,27 @@
+import Vue from 'vue';
+import './plugins/vuetify'
+import './plugins/vuetify';
+import sharedComponents from '@/plugins/sharedComponentsRequire';
+import App from './App.vue';
+import router from './routers';
+import {
+	store
+} from './store';
+import axios from 'axios';
+import '@/assets/sass/index.sass';
+
+sharedComponents();
+
+Vue.config.productionTip = false;
+const baseUrl =
+	process.env.NODE_ENV === 'development' ?
+	process.env.VUE_APP_SERVER_URL_DEV :
+	process.env.VUE_APP_SERVER_URL_PROD;
+
+axios.defaults.baseURL = baseUrl;
+
+new Vue({
+	router,
+	store,
+	render: h => h(App)
+}).$mount('#app');
