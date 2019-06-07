76c550e69d37287af81f11c0b4a2f1bc0ffe7d4f
supervueman <supervueman@gmail.com>
2019-06-07 18:19:15 +0400

    Add user card component

---
+++ dev/src/components/Shared/UserCard.vue
@@ -0,0 +1,54 @@
+<template lang="pug">
+  v-flex.py-2.px-2
+    v-card(:to="`/users/${id}`")
+      v-img(
+        :src="`/static/${avatar}`"
+        aspect-ratio="1"
+      )
+      v-flex.px-2 {{firstname}} {{lastname}}
+      v-flex Phone: {{phone}}
+      v-flex E-mail: {{email}}
+</template>
+
+
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+
+@Component
+export default class UserCard extends Vue {
+  @Prop({
+    default: ""
+  })
+  private firstname: string;
+
+  @Prop({
+    default: ""
+  })
+  private lastname: string;
+
+  @Prop({
+    default: ""
+  })
+  private avatar: string;
+
+  @Prop({
+    default: ""
+  })
+  private email: string;
+
+  @Prop({
+    default: ""
+  })
+  private phone: string;
+
+  @Prop({
+    default: ""
+  })
+  private id: string;
+
+  @Prop({
+    default: ""
+  })
+  private slug: string;
+}
+</script>
--- dev/src/pages/Users.vue
+++ dev/src/pages/Users.vue
@@ -1,5 +1,22 @@
 <template lang="pug">
-  
+  v-flex(class="mt-5")
+    v-card(
+      class="mx-auto"
+      tag="form"
+    )
+      v-card-title
+        h1(class="title") Users
+      v-card-text
+        v-layout.row.wrap(fill-height align-start justify-start)
+          user-card.xl3.lg3.md3.sm2.xs2(
+            firstname="Rinat"
+            lastname="Davlikamov"
+            avatar="avatar.jpg"
+            email="supervueman@gmail.com"
+            phone="+7 (937) 078-08-30"
+            id="1"
+            slug="supervueman"
+          )
 </template>
 
 <script lang="ts">
