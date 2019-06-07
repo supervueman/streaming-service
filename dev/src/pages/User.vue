271e4598dc6aae0fc9715b6f1796febec793f206
supervueman <supervueman@gmail.com>
2019-06-07 18:30:51 +0400

    Add user page

--- dev/src/pages/User.vue
+++ dev/src/pages/User.vue
@@ -1,10 +1,82 @@
 <template lang="pug">
-  div {{$route.params.id}}
+  v-card(
+      class="mx-auto"
+      max-width="500"
+      tag="form"
+    )
+      v-card-title
+        h1(class="title") User {{$route.params.id}}
+      v-card-text
+        v-text-field(
+          v-model="user.firstname"
+          label="Firstname:"
+          disabled
+        )
+        v-text-field(
+          v-model="user.lastname"
+          label="Lastname:"
+          disabled
+        )
+        v-text-field(
+          v-model="user.email"
+          label="E-mail:"
+          disabled
+        )
+        v-text-field(
+          v-model="user.phone"
+          label="Phone:"
+          disabled
+        )
+        v-text-field(
+          v-model="user.website"
+          label="Website:"
+          disabled
+        )
+        v-text-field(
+          v-model="user.facebook"
+          label="Facebook:"
+          disabled
+        )
+        v-text-field(
+          v-model="user.instagram"
+          label="Instagram:"
+          disabled
+        )
+        v-text-field(
+          v-model="user.vkontakte"
+          label="Vkontakte:"
+          disabled
+        )
+        v-text-field(
+          v-model="user.avatar"
+          label="Avatar:"
+          disabled
+        )
+        v-text-field(
+          v-model="user.content"
+          label="Content:"
+          disabled
+        )
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 
 @Component
-export default class User extends Vue {}
+export default class User extends Vue {
+  get user() {
+    return {
+      firstname: "",
+      lastname: "",
+      email: "",
+      phone: "",
+      website: "",
+      facebook: "",
+      instagram: "",
+      vkontakte: "",
+      avatar: "",
+      content: ""
+    };
+  }
+}
 </script>
